5a486b68eda79275512c10d91c9cfb11a5e2cdbb
USER <USER@DESKTOP-KP39UUC>
2023-06-03 17:45:24 +0530

    First Commit By ATE1

---
+++ DummyProGitHub/src/test/java/TestCases/TC1test.java
@@ -0,0 +1,16 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("TC1 passed",true);
+	}
+
+}
+
+
