8d784f764a732341cd2eb306cdd5a2bfc702433c
USER <USER@DESKTOP-KP39UUC>
2023-06-06 12:41:40 +0530

    TC4new branch commit

---
+++ DummyProGitHub/src/test/java/TestCases/TC4test.java
@@ -0,0 +1,12 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC4test {
+@Test
+public void d1()
+{
+	Reporter.log("Data Updated",true);
+}
+}
